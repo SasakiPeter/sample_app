d0cf707a833a2793c1980f66be77c316dd9398b5
SasakiPeter <anagentofthegod@gmail.com>
2022-06-20 18:23:09 +0900

    :+1: add comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
+  # なるほど、getのときは、urlをかけばいい
+  get "static_pages/home"
+  get "static_pages/help"
   root "application#hello"
 end
