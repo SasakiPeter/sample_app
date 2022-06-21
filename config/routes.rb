74fba0b5c455e24c544c81864eecfd16d5d505cf
SasakiPeter <anagentofthegod@gmail.com>
2022-06-21 16:07:00 +0900

    :+1: done 3.4

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  # rootのときは、関数を指定
+  # rootに指定したurlはroot_urlとして使える模様
+  root "static_pages#home"
   # なるほど、getのときは、urlをかけばいい
+  # こちらのurlはstatic_pages_home_urlとして使える
   get "static_pages/home"
   get "static_pages/help"
   get "static_pages/about"
   get "static_pages/contact"
-  root "application#hello"
 end
