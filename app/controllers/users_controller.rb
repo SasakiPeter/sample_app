f4c649febc68921d8704210ec9ba3d3bd4ba63a5
SasakiPeter <anagentofthegod@gmail.com>
2022-06-28 19:05:45 +0900

    :+1: done 7.3.3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,23 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
     # debugger
+  end
+
+  def create
+    # debugger
+    @user = User.new(user_params)
+    if @user.save
+    else
+      render "new"
+    end
+  end
+
+  private
+
+  def user_params
+    # params[:user]だと、他の属性に値を設定したrequestを受け取ってしまう
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
