d2bdfd988029bd9090cd7a6b2e6f422bc4294b14
SasakiPeter <anagentofthegod@gmail.com>
2022-07-01 22:46:21 +0900

    :+1: 11.3

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,12 +8,19 @@ class SessionsController < ApplicationController
     # local変数ではなく、instance変数にすることで、テストからassigns(:user)で参照できる
     @user = User.find_by(email: params[:session][:email].downcase)
     if @user && @user.authenticate(params[:session][:password])
-      # helperはcontrollerでも呼べる→なぜなら、include SessionsHelperしてるから
-      log_in @user
+      if @user.activated?
+        # helperはcontrollerでも呼べる→なぜなら、include SessionsHelperしてるから
+        log_in @user
 
-      params[:session][:remember_me] == "1" ? remember(@user) : forget(@user)
-      # 認証成功したら、個別ページにリダイレクトか、元のページに戻してあげる
-      redirect_back_or @user
+        params[:session][:remember_me] == "1" ? remember(@user) : forget(@user)
+        # 認証成功したら、個別ページにリダイレクトか、元のページに戻してあげる
+        redirect_back_or @user
+      else
+        message = "Account not activated. "
+        message += "Check your email for the activation link."
+        flash[:warning] = message
+        redirect_to root_url
+      end
     else
       # .nowをつけると、次のリクエストで消滅する
       flash.now[:danger] = "Invalid email/password combination"
